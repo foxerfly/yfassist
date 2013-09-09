ba192c76f67edec04d4c45a3af00f9daf0b6d964
edwin <wangwensi@gmail.com>
2013-09-09 14:59:56 +0800

    COMPANY0909

---
+++ BOM/src/com/edwin/bom/OrderNodeChildren.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edwin.bom;
+
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+
+/**
+ *
+ * @author John
+ */
+public class OrderNodeChildren extends Children.Keys<String> {
+
+    @Override
+    protected Node[] createNodes(String key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
