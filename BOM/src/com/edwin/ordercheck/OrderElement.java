8a5c443c06cde9145728e00cc0b5e69cebbbcb55
edwin <wangwensi@gmail.com>
2013-09-13 17:28:56 +0800

    COMPANY0913

---
+++ BOM/src/com/edwin/ordercheck/OrderElement.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edwin.ordercheck;
+
+/**
+ *
+ * @author John
+ */
+
+public class OrderElement {
+
+    public String getPh() {
+        return ph;
+    }
+
+    public void setPh(String ph) {
+        this.ph = ph;
+    }
+    
+    String ph;
+    
+    
+    
+    
+}
---
+++ BOM/src/com/edwin/ordercheck/checkAction.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edwin.ordercheck;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "基础资料",
+        id = "com.edwin.ordercheck.checkAction"
+        )
+@ActionRegistration(
+        displayName = "#CTL_checkAction"
+        )
+@ActionReference(path = "Menu/基础资料", position = 3333)
+@Messages("CTL_checkAction=checkAction")
+public final class checkAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
