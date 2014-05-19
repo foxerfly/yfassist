48196f32f77411026d9a86a41bf81fb443416085
Administrator <Administrator@WINDOWS-LZ0G2L2>
2014-05-19 14:35:21 +0800

    2014.5.19

--- SFC/src/com/edwin/my/LRP/MOCTA/UpdateMoctaTopComponent.java
+++ SFC/src/com/edwin/my/LRP/MOCTA/UpdateMoctaTopComponent.java
@@ -25,7 +25,7 @@ import org.openide.util.NbBundle.Messages;
         )
 @TopComponent.Registration(mode = "editor", openAtStartup = false)
 @ActionID(category = "生产", id = "com.edwin.my.LRP.MOCTA.UpdateMoctaTopComponent")
-@ActionReference(path = "Menu/生产" /*, position = 333 */)
+//@ActionReference(path = "Menu/生产" /*, position = 333 */)
 @TopComponent.OpenActionRegistration(
         displayName = "#CTL_UpdateMoctaAction",
         preferredID = "UpdateMoctaTopComponent"
